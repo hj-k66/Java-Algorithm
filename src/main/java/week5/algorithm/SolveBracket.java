2cfbf7941cb7a35fc4b45a665454446581928740
heejeong <hjx66@naver.com>
2022-10-21 10:33:57 +0900

    [programmers] 올바른 괄호 - 스택으로 풀기

--- src/main/java/week5/algorithm/SolveBracket.java
+++ src/main/java/week5/algorithm/SolveBracket.java
@@ -1,7 +1,25 @@
 package week5.algorithm;
 
+import java.util.Stack;
+
 public class SolveBracket {
     public boolean solution(String s){
+        Stack<Character> stack = new Stack<>();
+        for(int i = 0; i < s.length();i++){
+            char ch = s.charAt(i);
+            if(stack.isEmpty() && ch == ')'){
+                return false;
+            }
+            if(ch == '('){
+                stack.push(ch);
+            }else{
+                stack.pop();
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    public boolean solutionNotStack(String s){
         //스택 안쓰고 괄호 풀기
         while(s.indexOf("()") >= 0){
             s = s.replace("()","");

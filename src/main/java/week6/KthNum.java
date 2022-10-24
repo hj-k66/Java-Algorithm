3745525fda9b438c9b5a7f6cc445f4059720094a
heejeong <hjx66@naver.com>
2022-10-24 10:27:50 +0900

    add:[Programmers] k번째 수 우선순위 큐로 해결

--- src/main/java/week6/KthNum.java
+++ src/main/java/week6/KthNum.java
@@ -1,6 +1,7 @@
 package week6;
 
 import java.util.Arrays;
+import java.util.PriorityQueue;
 
 public class KthNum {
     public int[] solution(int[] array, int[][] commands) {
@@ -18,11 +19,28 @@ public class KthNum {
         return answer;
     }
 
+    public int[] solutionByQueue(int[] array, int[][] commands){
+        int[] answer = new int[commands.length];
+        int idx = 0;
+        for(int[] command : commands){
+            int result = 0;
+            PriorityQueue<Integer> pq = new PriorityQueue<>();
+            for(int i = command[0]-1; i < command[1];i++){
+                pq.add(array[i]);
+            }
+            for(int i = 0; i < command[2]; i++){
+                result = pq.poll();
+            }
+            answer[idx++] = result;
+        }
+        return answer;
+    }
+
     public static void main(String[] args) {
         int[] array = {1, 5, 2, 6, 3, 7, 4};
         int[][] commands = {{2, 5, 3}, {4, 4, 1},{1, 7, 3}};
         KthNum kthNum = new KthNum();
-        int[] answer = kthNum.solution(array,commands);
+        int[] answer = kthNum.solutionByQueue(array,commands);
         System.out.println(Arrays.toString(answer));
     }
 }

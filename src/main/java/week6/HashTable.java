f5243f5259dc1748592351a4a345c09c384523d3
heejeong <hjx66@naver.com>
2022-10-26 10:45:28 +0900

    [refactor] 해시 충돌 해결 - Node 클래스 추가

--- src/main/java/week6/HashTable.java
+++ src/main/java/week6/HashTable.java
@@ -1,21 +1,41 @@
 package week6;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class HashTable {
+    class Node {
+        private String key;
+        private Integer value;
+
+        public Node(String key, Integer value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public String getKey() {
+            return key;
+        }
+
+        public Integer getValue() {
+            return value;
+        }
+    }
+
     private int size = 10000;
-    private int[] table = new int[size];
+    private List<Node>[] table = new ArrayList[size];
 
     public HashTable() {
     }
 
     public HashTable(int size) {
         this.size = size;
-        this.table = new int[size];
+        this.table = new ArrayList[size];
     }
 
-    public int hash(String key){
+    public int hash(String key) {
         //key의 각 문자 ascii 코드값을 합침
         int asciiSum = 0;
         for (int i = 0; i < key.length(); i++) {
@@ -26,13 +46,24 @@ public class HashTable {
     }
 
     //데이터 저장
-    public void insert(String key, Integer value){
-        this.table[hash(key)] = value;
+    public void insert(String key, Integer value) {
+        int hashIdx = hash(key);
+        if (this.table[hashIdx] == null) {
+            this.table[hashIdx] = new ArrayList<>();
+        }
+        this.table[hashIdx].add(new Node(key, value));
     }
 
+
     //주어진 key에 해당하는 value 리턴
-    public int search(String key){
-        return this.table[hash(key)];
+    public Integer search(String key) {
+        List<Node> nodes = this.table[hash(key)];
+        for (Node node : nodes) {
+            if (key.equals(node.getKey())) {
+                return node.getValue();
+            }
+        }
+        return null;
     }
 
     public static void main(String[] args) {
@@ -40,12 +71,29 @@ public class HashTable {
                 "SubinKang", "KwanwunKo", "HyunseokKo", "KyoungdukKoo", "YeonjiGu", "SoyeonKown", "OhsukKwon", "GunwooKim", "KiheonKim", "NayeongKim", "DohyeonKim", "MinkyoungKim", "MinjiKim", "SanghoKim", "SolbaeKim", "YejinKim", "EungjunKim", "JaegeunKim", "JeonghyeonKim", "JunhoKim", "JisuKim", "kimjinah", "HaneulKim", "HeejungKim", "KimoonPark", "EunbinPark", "JeongHoonPark", "JeminPark", "TaegeunPark", "JiwonBae", "SeunggeunBaek", "JihwanByeon", "HeungseopByeon", "JeongHeeSeo", "TaegeonSeo", "SeeYunSeok", "SuyeonSeong", "SeyoelSon", "MinjiSong", "JinwooSong", "hyunboSim", "SominAhn", "JiyoungAhn", "ChangbumAn", "SoonminEom",
                 "HyeongsangOh", "SuinWoo", "JuwanWoo", "InkyuYoon", "GahyunLee", "DaonLee", "DohyunLee", "SanghunLee", "SujinLee", "AjinLee", "YeonJae", "HyeonjuLee", "HakjunYim", "SeoyunJang", "SeohyeonJang", "JinseonJang", "SujinJeon", "SeunghwanJeon", "DaehwanJung", "JaeHyunJeung", "HeejunJeong", "GukhyeonCho", "MunjuJo", "YejiJo", "ChanminJu", "MinjunChoi", "SujeongChoi", "SeunghoChoi", "AyeongChoi", "GeonjooHan", "JinhyuckHeo", "MinwooHwang", "SieunHwang",
                 "JunhaHwang"};
-        HashTable hashTable = new HashTable(200);
-        for(int i = 0; i < names.length; i++){
-            hashTable.insert(names[i],hashTable.hash(names[i]));
-        }
-        System.out.println(hashTable.search("HeejungKim"));
 
+        HashTable ht = new HashTable();
+        for (int i = 0; i < names.length; i++) {
+            ht.insert(names[i], ht.hash(names[i]));
+        }
+        ht.insert("Yoonseo", 1);
+        ht.insert("Seoyoon", 2);
+
+        int result = ht.search("Yoonseo");
+        if (result == 1) {
+            System.out.println("테스트 성공");
+        } else {
+            System.out.printf("테스트 실패 value:%d", result);
+        }
+
+        result = ht.search("Seoyoon");
+        if (result == 2) {
+            System.out.println("테스트 성공");
+        } else {
+            System.out.printf("테스트 실패 value:%d", result);
+        }
 
     }
+
+
 }

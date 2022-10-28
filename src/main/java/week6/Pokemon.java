84b1a18f92778261c68d1b2f206fb69b427489c3
heejeong <hjx66@naver.com>
2022-10-28 09:45:28 +0900

    [Programmers] 폰켓몬

---
+++ src/main/java/week6/Pokemon.java
@@ -0,0 +1,19 @@
+package week6;
+
+import java.util.HashSet;
+import java.util.Set;
+//선택 가능한 종류 파악 >> 종류 개수가 length/2보다 작으면 개수리턴, 아니면 length/2리턴
+public class Pokemon{
+    public int solution(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int num:nums) {
+            set.add(num);
+        }
+        int n = nums.length / 2;
+        if(set.size() < n){
+            return set.size();
+        }else{
+            return n;
+        }
+    }
+}

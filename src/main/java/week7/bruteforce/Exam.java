3a03de06023d108fbf742b1c8a090c4734c02258
heejeong <hjx66@naver.com>
2022-10-31 10:44:50 +0900

    feat: 프로그래머스 모의고사 완료 - max 점수 구하기 - max와 같으면 해당 학생번호 list에 append - List를 arr로 변환

--- src/main/java/week7/bruteforce/Exam.java
+++ src/main/java/week7/bruteforce/Exam.java
@@ -1,19 +1,54 @@
 package week7.bruteforce;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Exam {
+    public int getMax(int[] arr){
+        int maxValue = arr[0];
+        for(int i = 0; i < arr.length;i++){
+            if(maxValue < arr[i]){
+                maxValue = arr[i];
+            }
+        }
+        return maxValue;
+    }
+
     public int[] solution(int[] answers) {
-        int[] result = {};
-        int[] firstStudent = {1,2,3,4,5};
+        List<Integer> answer = new ArrayList<>();
+        int[] firstStudent = {1 ,2, 3, 4, 5};
         int[] secondStudent = {2, 1, 2, 3, 2, 4, 2, 5};
         int[] thirdStudent = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
 
-        //12345 패턴으로 찍는 학생이 몇 문제 맞췄는지
-        int firstStudentAnswerCnt = 0;
+        int[] result = {0,0,0};
+
         for(int i = 0; i < answers.length;i++){
-            if(answers[i]==firstStudent[i]){
-                firstStudentAnswerCnt++;
+            if(answers[i]==firstStudent[i%firstStudent.length]){
+                result[0]++;
+            }
+            if(answers[i]==secondStudent[i%secondStudent.length]){
+                result[1]++;
+            }
+            if(answers[i]==thirdStudent[i%thirdStudent.length]){
+                result[2]++;
             }
         }
-        return result;
+
+
+        int maxValue = getMax(result);
+        for(int i = 0; i < result.length;i++){
+            if(maxValue==result[i]){
+                answer.add(i+1);
+            }
+        }
+
+        //List를 array로 변환
+        int[] arr = new int[answer.size()];
+        for(int i = 0; i < answer.size();i++){
+            arr[i] = answer.get(i);
+        }
+        return arr;
     }
+
 }

ae2de404a1f148c2bf5259a579fa00a511f48e37
heejeong <hjx66@naver.com>
2022-11-02 10:38:03 +0900

    refactor: 에라토스테네스의 체 array로 변경

--- src/main/java/week7/bruteforce/PrimeEratosthenes.java
+++ src/main/java/week7/bruteforce/PrimeEratosthenes.java
@@ -1,27 +1,38 @@
 package week7.bruteforce;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 public class PrimeEratosthenes {
-    public static void main(String[] args) {
-        int n = 50;
-        List<Integer> numbers = new ArrayList<>();
-        for (int i = 2; i < n + 1; i++) {
-            numbers.add(i);
+    public int solution(int n) {
+        int[] numbers = new int[n-1];
+        boolean[] checks = new boolean[n-1];
+        int num = 2;
+        for (int i = 0; i < n-1 ; i++) {
+            numbers[i] = num++;
+            checks[i] = true;
         }
 
-        for (int num = 2; num * num < n; num++) {
-            for (int i = 0; i < numbers.size(); i++) {
-                if (numbers.get(i) % num == 0 && numbers.get(i) != num) {
-                    numbers.remove(i);
+        for (int k = 2; k * k < n; k++) {
+            for (int i = 0; i < numbers.length; i++) {
+                if(numbers[i] % k == 0 && numbers[i] != k){
+                    checks[i] = false;
                 }
             }
         }
+        //check가 true인 numbers[i] >> 소수
+        int answer = 0;
+        for (int i = 0; i < n-1 ; i++) {
+            if(checks[i]){
+                answer++;
+            }
+        }
+        return answer;
+    }
 
-
-        System.out.println(numbers.size());
-        System.out.println(numbers);
+    public static void main(String[] args) {
+        int n = 50;
+        PrimeEratosthenes pe = new PrimeEratosthenes();
+        System.out.println(pe.solution(n));
     }
 }

88cc950ff014d7b56478e674a317cf05a28d0d1c
heejeong <hjx66@naver.com>
2022-11-02 10:09:28 +0900

    feat: 에라토스테네스의 체 완료

--- src/main/java/week7/bruteforce/PrimeEratosthenes.java
+++ src/main/java/week7/bruteforce/PrimeEratosthenes.java
@@ -7,19 +7,21 @@ import java.util.List;
 public class PrimeEratosthenes {
     public static void main(String[] args) {
         int n = 50;
-        List<Integer> list = new ArrayList<>();
-        for(int i = 2; i < n+1;i++){
-            list.add(i);
+        List<Integer> numbers = new ArrayList<>();
+        for (int i = 2; i < n + 1; i++) {
+            numbers.add(i);
         }
-        Iterator<Integer> iterator = list.iterator();
-        while (iterator.hasNext()) {
-            int elem = iterator.next();
-            if (elem%2 == 0 && elem != 2) {
-                iterator.remove();
+
+        for (int num = 2; num * num < n; num++) {
+            for (int i = 0; i < numbers.size(); i++) {
+                if (numbers.get(i) % num == 0 && numbers.get(i) != num) {
+                    numbers.remove(i);
+                }
             }
         }
 
-        System.out.println(list.size());
-        System.out.println(list);
+
+        System.out.println(numbers.size());
+        System.out.println(numbers);
     }
 }

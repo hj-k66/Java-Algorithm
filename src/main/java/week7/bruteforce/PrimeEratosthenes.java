89ee5265d6c39671b26a3224e0f09194c88023f9
heejeong <hjx66@naver.com>
2022-11-03 10:12:39 +0900

    feat: 에라토스테네스의 체 2의 배수 제거된 결과 출력

--- src/main/java/week7/bruteforce/PrimeEratosthenes.java
+++ src/main/java/week7/bruteforce/PrimeEratosthenes.java
@@ -14,15 +14,25 @@ public class PrimeEratosthenes {
             nums[i] = i+2;
         }
 
+        int multipleOf = 2;
+        for(int i = 2; i < nums.length; i+=multipleOf){
+            checks[i] = false;
 
+        }
+
+        //checks가 true인 nums[i]만 출력
+        for(int i = 0; i < nums.length; i++){
+            if(checks[i]){
+                System.out.println(nums[i]);
+            }
+        }
 
         return answer;
     }
 
     public static void main(String[] args) {
         int n = 50;
-        for(int i = 2; i <= 100; i++ ){
-            System.out.printf("%d, %d\n", i,(i-1)*2);
-        }
+        PrimeEratosthenes pe = new PrimeEratosthenes();
+        pe.solution(n);
     }
 }

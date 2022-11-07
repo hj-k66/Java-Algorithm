7768e39675e9d70207e2635aee62bef563305358
heejeong <hjx66@naver.com>
2022-11-07 10:46:01 +0900

    feat: Programmers 나누어 떨어지는 숫자 배열 PriorityQueue 이용

--- src/main/java/week8/DivisibleArray.java
+++ src/main/java/week8/DivisibleArray.java
@@ -3,6 +3,7 @@ package week8;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.PriorityQueue;
 
 public class DivisibleArray {
     public int[] solution(int[] arr, int divisor) {
@@ -21,6 +22,25 @@ public class DivisibleArray {
         int[] result = new int[answer.size()];
         for (int i = 0; i < result.length; i++) {
             result[i] = answer.get(i);
+        }
+        return result;
+    }
+
+    //PriorityQueue이용
+    public int[] otherSolution(int[] arr, int divisor){
+        PriorityQueue<Integer> answer = new PriorityQueue<>();
+        for(int elem : arr){
+            if(elem % divisor == 0){
+                answer.add(elem);
+            }
+        }
+        if(answer.size() == 0) return new int[]{-1};
+
+        //array로 변경
+        int[] result = new int[answer.size()];
+        int idx = 0;
+        while(!answer.isEmpty()){
+            result[idx++] = answer.poll();
         }
         return result;
     }

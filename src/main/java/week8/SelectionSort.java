d86f8885bdc3a9c54285bcd0a4a29177532dc2ca
heejeong <hjx66@naver.com>
2022-11-09 09:52:40 +0900

    feat: interface, 람다 적용해 내림차순 선택 정렬 추가

--- src/main/java/week8/SelectionSort.java
+++ src/main/java/week8/SelectionSort.java
@@ -3,26 +3,47 @@ package week8;
 import java.util.Arrays;
 
 public class SelectionSort {
-    public static void swap(int[] arr, int i , int j){
+    public interface Compare{
+        boolean getCondition(int a, int b);
+    }
+    public void swap(int[] arr, int i , int j){
         int temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
     }
-    public static int findMinIndex(int[] arr, int i){
-        int minIdx = i;
+
+    public int findIndex(int[] arr, int i, Compare compare){
+        int idx = i;
         for(int j = i+1; j < arr.length;j++){
-            if(arr[minIdx] > arr[j]){
-                minIdx = j;
+            if(compare.getCondition(arr[idx], arr[j])){
+                idx = j;
             }
         }
-        return minIdx;
+        return idx;
     }
+
+    public int findMinIndex(int[] arr, int i){
+        return findIndex(arr, i, (a, b) -> a > b);
+    }
+    public int findMaxIndex(int[] arr, int i){
+        return findIndex(arr,i, (a, b) -> a < b);
+    }
+
+
     public static void main(String[] args) {
         int[] arr = new int[]{2, 7, 4, 9, 10, 223, 111, 23, 3, 39};
+        SelectionSort selectionSort = new SelectionSort();
         for (int i = 0; i < arr.length; i++) {
-            int minIdx = findMinIndex(arr,i);
-            swap(arr,i,minIdx);
+            int minIdx = selectionSort.findMinIndex(arr,i);
+            selectionSort.swap(arr,i,minIdx);
         }
         System.out.println(Arrays.toString(arr));
+
+        int[] arr2 = new int[]{2, 7, 4, 9, 10, 223, 111, 23, 3, 39};
+        for (int i = 0; i < arr2.length; i++) {
+            int maxIdx = selectionSort.findMaxIndex(arr2,i);
+            selectionSort.swap(arr2,i,maxIdx);
+        }
+        System.out.println(Arrays.toString(arr2));
     }
 }

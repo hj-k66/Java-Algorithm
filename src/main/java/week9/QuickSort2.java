8f84e16930036c0ef0fece5c9b416a389b300299
heejeong <hjx66@naver.com>
2022-11-17 10:20:33 +0900

    feat: 퀵정렬 Array로 구현 완료

--- src/main/java/week9/QuickSort2.java
+++ src/main/java/week9/QuickSort2.java
@@ -1,10 +1,12 @@
 package week9;
 
+import java.util.Arrays;
+
 public class QuickSort2 {
     public int[] sort(int[] arr, int startIdx, int endIdx) {
-        int pivot = arr[arr.length / 2];
         int leftIdx = startIdx;
         int rightIdx = endIdx;
+        int pivot = arr[(startIdx+endIdx) / 2];
 
         while (leftIdx <= rightIdx) {
             while (arr[leftIdx] < pivot) {
@@ -22,8 +24,17 @@ public class QuickSort2 {
                 rightIdx -= 1;
             }
         }
+        if(startIdx<rightIdx) sort(arr,startIdx,rightIdx);
+        if(leftIdx < endIdx) sort(arr,leftIdx,endIdx);
 
         return arr;
 
+    }
+
+    public static void main(String[] args) {
+        var arr = new int[]{20, 18, 5, 19, 40, 50, 5, 25};
+        QuickSort2 quickSort2 = new QuickSort2();
+        var result = quickSort2.sort(arr,0,arr.length-1);
+        System.out.println(Arrays.toString(result));
     }
 }

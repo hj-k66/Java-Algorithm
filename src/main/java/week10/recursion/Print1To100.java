279b3eb3c2406c07b6c773f7fd6633f2d7384d3b
heejeong <hjx66@naver.com>
2022-11-22 09:19:33 +0900

    feat: 1~100까지 숫자 출력 - 재귀로 구현

---
+++ src/main/java/week10/recursion/Print1To100.java
@@ -0,0 +1,13 @@
+package week10.recursion;
+
+public class Print1To100 {
+    public static void print1To100(int n){
+        if(n > 100) return;
+        System.out.println(n);
+        print1To100(n+1);
+    }
+
+    public static void main(String[] args) {
+        print1To100(1);
+    }
+}

6dd44c26e312dbca819aff2aae2dec4808872f50
heejeong <hjx66@naver.com>
2022-11-23 09:31:59 +0900

    refactor: CodeUp 1853 재귀로 1부터 n까지 합 리턴하기, static 변수 없이 구현

--- src/main/java/week10/recursion/C_1853.java
+++ src/main/java/week10/recursion/C_1853.java
@@ -5,17 +5,15 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class C_1853 {
-    static int sum = 0;
-    public static void plus(int n){
-        if(n == 0) return;
-       sum += n;
-       plus(n-1);
+    public static int plus(int n){
+        if(n == 0) return 0;
+        return n + plus(n-1);
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        plus(n);
+        int sum = plus(n);
         System.out.println(sum);
     }
 }

c7c81b7fd103e188615eb6fa1fa5b67c2454323b
heejeong <hjx66@naver.com>
2022-11-23 09:45:19 +0900

    feat: CodeUp 1912 : (재귀함수) 팩토리얼 계산

---
+++ src/main/java/week10/recursion/Factorial.java
@@ -0,0 +1,18 @@
+package week10.recursion;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Factorial {
+    public static int factorial(int n) {
+        if (n == 1) return 1;
+        return n * factorial(n - 1);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        System.out.println(factorial(n));
+    }
+}

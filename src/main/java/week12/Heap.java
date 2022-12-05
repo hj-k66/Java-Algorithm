63e5742734a4cc90b1787535c5c9618ed802611e
heejeong <hjx66@naver.com>
2022-12-05 10:10:48 +0900

    feat: 자식들의 idx 구하는 함수 구현

--- src/main/java/week12/Heap.java
+++ src/main/java/week12/Heap.java
@@ -3,13 +3,20 @@ package week12;
 import java.util.*;
 
 public class Heap {
+    public static int[] getChildIdx(int parentIdx) {
+        int[] child = new int[2];
+        child[0] = parentIdx * 2 + 1;
+        child[1] = parentIdx * 2 + 2;
+        return child;
+    }
+
     public static void main(String[] args) {
-        int[] arr = {6,5,7,8};
-        for (int i = arr.length-1; i > 0 ; i--) {
-            int parent = arr[i/2];
-            if(arr[i/2] < arr[i]){
-                int temp = arr[i/2];
-                arr[i/2] = arr[i];
+        int[] arr = {6, 5, 7, 8};
+        for (int i = arr.length - 1; i > 0; i--) {
+            int parentIdx = i / 2;
+            if (arr[parentIdx] < arr[i]) {
+                int temp = arr[parentIdx];
+                arr[parentIdx] = arr[i];
                 arr[i] = temp;
             }
         }
